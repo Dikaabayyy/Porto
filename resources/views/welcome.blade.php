d04aa815234d1a2f99b7ff67ba6d5de27fa850d9
Andhika Bayu <andhikabayu.andhik@gmail.com>
2024-01-14 12:56:19 +0900

    update port

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
         @vitereactrefresh
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
 
         <!-- Styles -->
         <style>
