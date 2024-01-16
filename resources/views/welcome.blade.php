e507dfb403b1c4facc8311d3074a4fb554fb6bf4
Andhika Bayu <andhikabayu.andhik@gmail.com>
2024-01-16 18:33:39 +0900

    update port

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <link rel="stylesheet" href="{{ asset('build/assets/app-lhA9k1qk.css') }}">
-        <script src="{{ asset('build/assets/app-5m7foVeW.js') }}"></script>
+        <script src="{{ asset('build/assets/app-3kKWeomu.js') }}"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
